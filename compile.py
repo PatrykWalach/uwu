8f8545c050857ec51520dc1c54c6c8b038ecc884
PatrykWalach <patryk_walach_github@outlook.com>
2021-12-01 12:30:17 +0100

    feat

---
+++ compile.py
@@ -0,0 +1,13 @@
+
+from algorithm_j import Context
+import terms
+
+
+def compile(ctx: Context, exp: terms.AstTree) -> str:
+    match exp:
+        case terms.ELiteral(value=str() as value):
+            return value
+        case terms.ELiteral(value=float() as value):
+            return f"{value}"
+        case _:
+            return NotImplemented
