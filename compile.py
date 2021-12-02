1a864ec9bf20ab8535421fd130ec5b0e05be3320
PatrykWalach <patryk_walach_github@outlook.com>
2021-12-02 14:32:56 +0100

    fix

--- compile.py
+++ compile.py
@@ -6,7 +6,7 @@ import terms
 def compile(ctx: Context, exp: terms.AstTree) -> str:
     match exp:
         case terms.ELiteral(value=str() as value):
-            return value
+            return f'"{value}"'
         case terms.ELiteral(value=float() as value):
             return f"{value}"
         case _:
