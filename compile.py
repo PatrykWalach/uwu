b068491c0e512b3232d747cc70be3c607a3f78cb
PatrykWalach <patryk_walach_github@outlook.com>
2021-12-03 13:58:07 +0100

    feat

--- compile.py
+++ compile.py
@@ -9,5 +9,11 @@ def compile(ctx: Context, exp: terms.AstTree) -> str:
             return f'"{value}"'
         case terms.ELiteral(value=float() as value):
             return f"{value}"
+        case terms.EVariableDeclaration(terms.EIdentifier(id), init):
+            return f"{id}={compile(ctx, init)};"
+        case terms.EDo(body):
+            body = [compile(ctx, expr) for expr in body]
+            body[-1] = 'return ' + body[-1]
+            return "(()=>{"+"".join(body)+"})()"
         case _:
             return NotImplemented
